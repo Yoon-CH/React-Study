83b42a2b860730546df11a21117d24be5191edc5
윤창현 <changh950@gmail.com>
2022-04-08 16:48:48 +0900

    ✨ react Twitter firebase를 활용한 데이터 생성

--- src/Components/ReactTwitter/TwitterHome.jsx
+++ src/Components/ReactTwitter/TwitterHome.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../../firebaseData';
+import { addDoc, collection } from 'firebase/firestore';
 
 const TwitterHome = () => {
-  return <div>home</div>;
+  const [nweet, setNweet] = useState('');
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, 'nweets'), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+    setNweet('');
+  };
+
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          onChange={onChange}
+          placeholder="What's on Your Mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </>
+  );
 };
 
 export default TwitterHome;
