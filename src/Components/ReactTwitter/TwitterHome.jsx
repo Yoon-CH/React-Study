4e1db721e152dad980c569ef9d06fb9b172e7685
윤창현 <changh950@gmail.com>
2022-04-09 16:05:41 +0900

    ✨ react Twitter firebase를 활용한 데이터 불러오기

--- src/Components/ReactTwitter/TwitterHome.jsx
+++ src/Components/ReactTwitter/TwitterHome.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { dbService } from '../../firebaseData';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import Navigation from './Navigation';
 
-const TwitterHome = () => {
+const TwitterHome = isLoggedIn => {
   const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
 
   const onSubmit = async e => {
     e.preventDefault();
@@ -26,8 +28,24 @@ const TwitterHome = () => {
     setNweet(value);
   };
 
+  const getNweets = async () => {
+    const q = query(collection(dbService, 'nweets'));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(doc => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets(prev => [nweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+
   return (
     <>
+      {isLoggedIn && <Navigation />}
       <form onSubmit={onSubmit}>
         <input
           type="text"
@@ -38,6 +56,13 @@ const TwitterHome = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map(data => (
+          <div key={data.id}>
+            <h4>{data.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
